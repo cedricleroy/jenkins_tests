c6fa0ecb43dd4d20b7fe93ae4cd7f68791f56857
Cedric Leroy <cedie73@hotmail.fr>
2016-12-08 12:15:34 -0600

    installing requirements after activating venv

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ fi
 
 PYENV_HOME=$WORKSPACE/venv/
 
-pip install --quiet -r requirements.txt -t $WORKSPACE
+#pip install --quiet -r requirements.txt -t $WORKSPACE
 
 #touch $WORKSPACE/google/__init__.py
 
 virtualenv -p python2.7 $PYENV_HOME
 . $PYENV_HOME/bin/activate
-
+pip install --quiet -r requirements.txt
 pip install --quiet pylint
 pip install --quiet pytest
 
