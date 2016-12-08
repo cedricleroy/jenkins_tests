5d9ffdf60bb26e0666e7775d0c315df88329b906
Cedric Leroy <cedie73@hotmail.fr>
2016-12-08 12:19:53 -0600

    adding log

--- build.sh
+++ build.sh
@@ -6,12 +6,17 @@ fi
 
 PYENV_HOME=$WORKSPACE/venv/
 
+which python
+
 #pip install --quiet -r requirements.txt -t $WORKSPACE
 
 #touch $WORKSPACE/google/__init__.py
 
 virtualenv -p python2.7 $PYENV_HOME
 . $PYENV_HOME/bin/activate
+
+which python
+
 pip install --quiet -r requirements.txt
 pip install --quiet pylint
 pip install --quiet pytest
